098cbb9280a5d7094138faf14782793f98d741a2
3100 <sugar16g@gmail.com>
2014-09-20 16:27:46 +0900

    add some spec files for 3-2-nfa/

--- 3-2-nfa/nfa.rb
+++ 3-2-nfa/nfa.rb
@@ -1,27 +1,6 @@
-require 'set'
-
-class NFARulebook < Struct.new(:rules)
-  def next_states(states, character)
-    states.flat_map { |state| follow_rules_for(state, character) }.to_set
-  end
-
-  def follow_free_moves(states)
-    more_states = next_states(states, nil)
-    if more_states.subset?(states)
-      states
-    else
-      follow_free_moves(states + more_states)
-    end
-  end
-
-  def follow_rules_for(state, character)
-    rules_for(state, character).map(&:follow)
-  end
-
-  def rules_for(state, character)
-    rules.select { |rule| rule.applies_to?(state, character) }
-  end
-end
+require_relative '../3-1-dfa/dfa'
+require_relative 'nfa-design'
+require_relative 'nfa-rulebook'
 
 class NFA < Struct.new(:current_states, :accept_states, :rulebook)
   def accepting?
@@ -40,15 +19,5 @@ class NFA < Struct.new(:current_states, :accept_states, :rulebook)
     string.chars.each do |character|
       read_character(character)
     end
-  end
-end
-
-class NFADesign < Struct.new(:start_state, :accept_states, :rulebook)
-  def accepts?(string)
-    to_nfa.tap { |nfa| nfa.read_string(string) }.accepting?
-  end
-
-  def to_nfa
-    NFA.new(Set[start_state], accept_states, rulebook)
   end
 end
---
+++ 3-2-nfa/spec/nfa-design_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe NFADesign, "#accepts?" do
+  before :all do
+    rulebook = NFARulebook.new([
+                                FARule.new(1, nil, 2), FARule.new(1, nil, 4),
+                                FARule.new(2, 'a', 3),
+                                FARule.new(3, 'a', 2),
+                                FARule.new(4, 'a', 5),
+                                FARule.new(5, 'a', 6),
+                                FARule.new(6, 'a', 4)
+    ])
+    @nfa_design = NFADesign.new(1, [2, 4], rulebook)
+  end
+
+  it "returns true for 'aa'" do
+    @nfa_design.accepts?('aa').should be_true
+  end
+
+  it "returns true for 'aaa'" do
+    @nfa_design.accepts?('aaa').should be_true
+  end
+
+  it "returns false for 'aaaaa'" do
+    @nfa_design.accepts?('aaaaa').should be_false
+  end
+
+  it "returns true for 'aaaaaa'" do
+    @nfa_design.accepts?('aaaaaa').should be_true
+  end
+end
